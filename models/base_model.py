470b3ae4e5d728eed5b24bcc4d64c2c4499f534e
eslam <eslam@org.com>
2024-01-14 16:21:32 -0500

    mm

--- models/base_model.py
+++ models/base_model.py
@@ -1,60 +1,77 @@
 #!/usr/bin/python3
-"""this is the base model."""
-
-from uuid import uuid4
+"""Unittest for base model
+"""
+import unittest
+from models.base_model import BaseModel
+from models import storage
 from datetime import datetime
-import models
 
 
-class BaseModel:
-    """BaseModel class."""
+class TestBaseModel(unittest.TestCase):
+    """
+    test class for the max_integer() function.
+    """
+    base = BaseModel()
+    base.name = "My First Model"
+    base.my_number = 89
 
-    def __init__(self, *args, **kwargs):
-        """Instance Constructor.
-
-        args
-        id:unique identifier.
-        created_at: date created at.
-        updated_at: date updated at.
+    def test_create_instance_without_kwargs(self):
         """
-        if not kwargs:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-        else:
-            for k, v in kwargs.items():
-                if k != "__class__":
-                    if k == "updated_at":
-                        self.updated_at = datetime.fromisoformat(v)
-                    elif k == "created_at":
-                        self.created_at = datetime.fromisoformat(v)
-                    else:
-                        setattr(self, k, v)
-
-    def __str__(self):
-        """__str__.
-
-        return string Representation.
+        create an instance of class without kwargs
         """
-        return f"[{self.__class__.__name__}] ({self.id}) <{self.__dict__}>"
+        self.assertIsInstance(self.base, BaseModel)
+        self.assertIsInstance(self.base.id, str)
+        self.assertIsInstance(self.base.created_at, datetime)
+        self.assertIsInstance(self.base.updated_at, datetime)
+        self.assertEqual(self.base.name, "My First Model")
+        self.assertEqual(self.base.my_number, 89)
 
-    def save(self):
-        """save.
-
-        update atr updated_at
+    def test_create_instance_with_kwargs(self):
         """
-        self.updated_at = datetime.now()
-        models.storage.save()
-
-    def to_dict(self):
-        """To_dict.
-
-        Returns:
-            dict: dictionary representation.
+        create an instance of class using kwargs
         """
-        my_class_dict = self.__dict__.copy()
-        my_class_dict["__class__"] = self.__class__.__name__
-        my_class_dict["updated_at"] = self.updated_at.isoformat()
-        my_class_dict["created_at"] = self.created_at.isoformat()
-        return my_class_dict
+        my_base_json = self.base.to_dict()
+        new_base = BaseModel(**my_base_json)
+        self.assertIsInstance(new_base, BaseModel)
+        self.assertIsInstance(new_base.id, str)
+        self.assertIsInstance(new_base.created_at, datetime)
+        self.assertIsInstance(new_base.updated_at, datetime)
+        self.assertEqual(new_base.name, "My First Model")
+        self.assertEqual(new_base.my_number, 89)
+        self.assertNotEqual(new_base, self.base)
+        self.assertDictEqual(new_base.__dict__, self.base.__dict__)
+
+    def test_to_dict(self):
+        """
+            test to_dict class method
+        """
+        to_dict_returned_dict = self.base.to_dict()
+        expected_dic = self.base.__dict__.copy()
+        expected_dic["__class__"] = self.base.__class__.__name__
+        expected_dic["updated_at"] = self.base.updated_at.isoformat()
+        expected_dic["created_at"] = self.base.created_at.isoformat()
+        self.assertDictEqual(expected_dic, to_dict_returned_dict)
+
+    def test_save(self):
+        """"
+            test save class method
+        """
+        before_update_time = self.base.updated_at
+        self.base.my_number = 90
+        self.base.save()
+        after_update_time = self.base.updated_at
+        self.assertNotEqual(before_update_time, after_update_time)
+        # all_objects = storage.all()
+        # new_number = all_objects[self.base.__class__.__name__ +
+        #                          "." + self.base.id]["my_number"]
+        # self.assertEqual(new_number, 90)
+
+    def test_str(self):
+        """
+            test str method
+
+            check for string representaion
+        """
+        n = self.base.__class__.__name__
+        expected_str = f"[{n}] ({self.base.id}) <{self.base.__dict__}>"
+        self.assertEqual(self.base.__str__(), expected_str)
